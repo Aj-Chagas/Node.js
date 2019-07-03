874e6689222fd58d874a2b6e1cfff979deafa731
Anderson Chagas <aj_chagas@hotmail.com>
2019-07-03 14:33:03 -0300

    add comentario

--- Node/instanciaUnica.js/instanciaUnica.js
+++ Node/instanciaUnica.js/instanciaUnica.js
@@ -1,4 +1,4 @@
-// O node faz cache, ou seja, o varavel valor vai para a cache 
+// O node faz cache, ou seja, o varavel valor vai para a cache e se o node encontra ela na cache ela vai sempre usar a mesma instancia
 
 module.exports = {
     valor: 1,
